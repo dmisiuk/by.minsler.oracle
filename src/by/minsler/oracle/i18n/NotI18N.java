ae6cd294510b140cb9ada356a65df8e113bb98ad
Dzmitry Misiuk <minsler.by@gmail.com>
2012-06-25 22:30:06 +0300

    Added Not i18n sample app

---
+++ src/by/minsler/oracle/i18n/NotI18N.java
@@ -0,0 +1,11 @@
+package by.minsler.oracle.i18n;
+
+public class NotI18N {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+		System.out.println("How are you?");
+		System.out.println("Good bye");
+	}
+}
